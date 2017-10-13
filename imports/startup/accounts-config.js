6c9bc3e85d4960facd2f64d23f6e6912872d57ed
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2017-10-13 10:36:39 +0700

    edit whitespaces

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,10 +1,10 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.ui.config({
- passwordSignupFields: 'USERNAME_ONLY',
+    passwordSignupFields: 'USERNAME_ONLY',
 });
 
 // Disable sign up
 Accounts.config({
     forbidClientAccountCreation : true
-  });
+});

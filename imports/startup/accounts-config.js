d59167fbdb74c1628b466b5aae9cb436d0d7680f
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2017-10-13 09:48:54 +0700

    hide body if not logged in AND disable new user sign up

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -3,3 +3,8 @@ import { Accounts } from 'meteor/accounts-base';
 Accounts.ui.config({
  passwordSignupFields: 'USERNAME_ONLY',
 });
+
+// Disable sign up
+Accounts.config({
+    forbidClientAccountCreation : true
+  });

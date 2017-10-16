f3d9abd8c29a3a3340cb812ac9ad6e6ef963b4e0
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2017-10-16 13:47:37 +0700

    added sb admin WITHOUT routing

--- client/main.js
+++ client/main.js
@@ -1,2 +1,13 @@
 import '../imports/startup/accounts-config.js';
-import '../imports/ui/blank.js';
+
+import "../imports/vendor/bootstrap/css/bootstrap.min.css";
+import "../imports/vendor/metisMenu/metisMenu.min.css";
+import "../imports/dist/css/sb-admin-2.css";
+import "../imports/vendor/font-awesome/css/font-awesome.min.css";
+
+import '../imports/ui/body.js';
+
+import "../imports/vendor/jquery/jquery.min.js";
+import "../imports/vendor/bootstrap/js/bootstrap.min.js";
+import "../imports/vendor/metisMenu/metisMenu.min.js";
+import "../imports/dist/js/sb-admin-2.js";
--- imports/ui/blank.js
+++ imports/ui/blank.js
@@ -1,11 +1,1 @@
-import "../vendor/bootstrap/css/bootstrap.min.css";
-import "../vendor/metisMenu/metisMenu.min.css";
-import "../dist/css/sb-admin-2.css";
-import "../vendor/font-awesome/css/font-awesome.min.css";
-
 import './blank.html';
-
-import "../vendor/jquery/jquery.min.js";
-import "../vendor/bootstrap/js/bootstrap.min.js";
-import "../vendor/metisMenu/metisMenu.min.js";
-import "../dist/js/sb-admin-2.js";

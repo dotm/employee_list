127954c7b39ed2dedc9de5d258fa9a48b3c37473
dotm <desde.el.mar96@gmail.com>
2017-10-18 13:54:42 +0700

    add client side validation for login user form

--- imports/ui/pages/login.js
+++ imports/ui/pages/login.js
@@ -1,1 +1,22 @@
+import { Template } from 'meteor/templating';
+
 import './login.html';
+
+Template.login.onRendered(function(){
+    $('form').validate({
+        rules: {
+            email: {
+              required: true,
+              email: true
+            },
+            password: 'required',
+          },
+          messages: {
+            email: {
+                required: "Please specify your email address",
+                email: "Your email address must be in the format of name@domain.com"
+            },
+            password: "Please specify your password",
+        }
+    })
+});

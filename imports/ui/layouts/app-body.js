edb3c5ce59507ec747b23cba4139a55d5a91f7cf
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2017-10-20 14:49:54 +0700

    clean up before finishing project

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Meteor } from 'meteor/meteor'
 
 import './app-body.html';
 
@@ -8,5 +9,17 @@ Template.App_body.events({
         event.preventDefault();
         Meteor.logout();
         FlowRouter.go('App.home');
+    },
+    'click a[href=login], a.login': function(event){
+        if(Meteor.user()){
+            event.preventDefault();
+            alert("You are already logged in.")
+        }
+    },
+    'click a[href=register], a.register': function(event){
+        if(Meteor.user()){
+            event.preventDefault();
+            alert("Please log out first.")
+        }
     }
 });

ffbf2c3ad9e0c4dd404455746e9762d4a1aff895
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2017-10-25 14:37:46 +0700

    prepare UI for lazy loading

--- imports/ui/pages/display.js
+++ imports/ui/pages/display.js
@@ -1,1 +1,25 @@
+import { Template } from 'meteor/templating';
+
 import './display.html';
+import {Employees} from '../../api/employees.js';
+
+Template.display.onCreated(function(){
+    Meteor.subscribe('employees')
+    Meteor.subscribe('images')
+});
+  
+Template.display.helpers({
+    employees(){
+      return Employees.find({});
+    },
+})
+
+Template.employeeDiv.helpers({
+    formatDate(date){
+        return date.toLocaleDateString('en',{
+            day:'numeric',
+            month:'short',
+            year:'numeric'
+        });
+    },
+})
